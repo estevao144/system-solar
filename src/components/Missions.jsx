c29f79db2c4944808c8a9b336154ae8d92eb9c1a
Estevao <estevo144@gmail.com>
2022-08-18 18:25:49 -0300

    questao 9 feita

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -4,9 +4,9 @@ import Title from './Title';
 class Missions extends React.Component {
   render() {
     return (
-      
-      <div data-testid="missions" >
-      <Title headline="Missões" />
+
+      <div data-testid="missions">
+        <Title headline="Missões" />
       </div>
     );
   }
