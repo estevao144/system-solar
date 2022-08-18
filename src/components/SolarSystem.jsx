980c51448250ef4a577ad5c8e5237c4c3dcd3e14
Estevao <estevo144@gmail.com>
2022-08-18 18:06:20 -0300

    questao 6 resolvida

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import Planetcard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((key) => (
+          <Planetcard
+            planetName={ key.name }
+            planetImage={ key.image }
+            key={ key.name }
+          />
+        ))}
       </div>
-
     );
   }
 }
