8df1f9260861c1cbf6adc1f894eefc3c95ca3b1c
Estevao <estevo144@gmail.com>
2022-08-18 18:11:03 -0300

    questao7 ok

---
+++ src/components/Missions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+
+export default Missions;
